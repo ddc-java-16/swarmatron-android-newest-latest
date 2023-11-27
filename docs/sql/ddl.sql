09cb99ca60967739c9d601f9392f93346fbb7636
che money <cmelendez15@cnm.edu>
2023-11-27 09:13:23 -0700

    updated docs

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -1,9 +1,63 @@
 -- Generated 2023-11-26 18:24:58-0700 for database version 1
 
-CREATE TABLE IF NOT EXISTS `user` (`user_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `created` INTEGER NOT NULL, `display_name` TEXT NOT NULL COLLATE NOCASE);
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `user_id`
+    INTEGER
+    PRIMARY
+    KEY
+    AUTOINCREMENT
+    NOT
+    NULL,
+    `created`
+    INTEGER
+    NOT
+    NULL,
+    `display_name`
+    TEXT
+    NOT
+    NULL
+    COLLATE
+    NOCASE
+);
 
 CREATE UNIQUE INDEX IF NOT EXISTS `index_user_display_name` ON `user` (`display_name`);
 
-CREATE TABLE IF NOT EXISTS `preset` (`preset_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `user_id` INTEGER NOT NULL, `preset_name` TEXT, `filter_position` REAL NOT NULL, `spread_ribbon_position` REAL NOT NULL, `waveform_selection` REAL NOT NULL, `noiseamount` REAL NOT NULL, `created` INTEGER NOT NULL);
+CREATE TABLE IF NOT EXISTS `preset`
+(
+    `preset_id`
+    INTEGER
+    PRIMARY
+    KEY
+    AUTOINCREMENT
+    NOT
+    NULL,
+    `user_id`
+    INTEGER
+    NOT
+    NULL,
+    `preset_name`
+    TEXT,
+    `filter_position`
+    REAL
+    NOT
+    NULL,
+    `spread_ribbon_position`
+    REAL
+    NOT
+    NULL,
+    `waveform_selection`
+    REAL
+    NOT
+    NULL,
+    `noiseamount`
+    REAL
+    NOT
+    NULL,
+    `created`
+    INTEGER
+    NOT
+    NULL
+);
 
 CREATE UNIQUE INDEX IF NOT EXISTS `index_preset_preset_name` ON `preset` (`preset_name`);
